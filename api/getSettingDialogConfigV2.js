31d397aeb901ff3aa4d27afe13cfc9764702757c
memory971027 <47736423+memory971027@users.noreply.github.com>
2025-07-06 02:44:08 +0800

    Update getSettingDialogConfigV2.js

--- api/getSettingDialogConfigV2.js
+++ api/getSettingDialogConfigV2.js
@@ -1,11 +1,16 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('X-D-I', 'tRY84woR9jliRWefUICkUxZv5j3o12MOHnS0nAp7XQs');
-  res.setHeader('X-W-I', 'ChHsh9+LpfWCCRto9E3133NZUM7PgI1TJvsl1dsA9LU=');
-  res.setHeader('X-C-M', 'XTeuJEeiVe5160gIP5GHYA==');
-  res.status(200).json({
-    code: 0,
-    data: "getSettingDialogConfigV28888888888888888888888888888",
-    message: null
-  });
-}
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-D-I': 'tRY84woR9jliRWefUICkUxZv5j3o12MOHnS0nAp7XQs',
+      'X-W-I': 'ChHsh9+LpfWCCRto9E3133NZUM7PgI1TJvsl1dsA9LU=',
+      'X-C-M': 'XTeuJEeiVe5160gIP5GHYA=='
+    },
+    body: JSON.stringify({
+      code: 0,
+      data: "getSettingDialogConfigV2",
+      message: null
+    })
+  };
+};
