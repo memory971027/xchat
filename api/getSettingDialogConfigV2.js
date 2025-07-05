4199bb44f77e0d41f6d68be26925a600be9be75c
memory971027 <47736423+memory971027@users.noreply.github.com>
2025-07-06 03:47:57 +0800

    Update getSettingDialogConfigV2.js

--- api/getSettingDialogConfigV2.js
+++ api/getSettingDialogConfigV2.js
@@ -1,6 +1,16 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello, world!' })
+    headers: {
+      'Content-Type': 'application/json',
+      'X-D-I': 'tRY84woR9jliRWefUICkUxZv5j3o12MOHnS0nAp7XQs',
+      'X-W-I': 'ChHsh9+LpfWCCRto9E3133NZUM7PgI1TJvsl1dsA9LU=',
+      'X-C-M': 'XTeuJEeiVe5160gIP5GHYA=='
+    },
+    body: JSON.stringify({
+      code: 0,
+      data: "getSettingDialogConfigV2",
+      message: null
+    })
   };
 };
