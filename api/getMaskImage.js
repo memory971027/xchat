8f959012717b414f3a6a6351c06fb8acc1c72351
岳芳 <memory971027@vip.qq.com>
2025-07-07 17:18:07 +0800

    图片更改

--- api/getMaskImage.js
+++ api/getMaskImage.js
@@ -1,21 +1,35 @@
-const fs = require("fs");
-const path = require("path");
+const https = require("https");
 
 exports.handler = async () => {
-  const imagePath = path.resolve(__dirname, "../public/mask.jpg");
-  const imageBuffer = fs.readFileSync(imagePath);
+  const imageUrl = "https://wchats.netlify.app/mask.jpg"; // 图片放在 public 下，对外可访问
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Disposition": "inline; filename=mask.jpg",
-      "Content-Type": "image/jpeg",
-      "Content-Length": imageBuffer.length.toString(),
-      "Cache-Control": "no-cache",
-      "Vary": "Accept-Encoding",
-      "Server": "Werkzeug/2.0.3 Python/3.6.8"
-    },
-    body: imageBuffer.toString("base64"),
-    isBase64Encoded: true,
-  };
+  return new Promise((resolve, reject) => {
+    https.get(imageUrl, (res) => {
+      let data = [];
+
+      res.on("data", (chunk) => data.push(chunk));
+      res.on("end", () => {
+        const imageBuffer = Buffer.concat(data);
+
+        resolve({
+          statusCode: 200,
+          headers: {
+            "Content-Disposition": "inline; filename=mask.jpg",
+            "Content-Type": "image/jpeg",
+            "Content-Length": imageBuffer.length.toString(),
+            "Cache-Control": "no-cache",
+            "Vary": "Accept-Encoding",
+            "Server": "Werkzeug/2.0.3 Python/3.6.8"
+          },
+          body: imageBuffer.toString("base64"),
+          isBase64Encoded: true,
+        });
+      });
+    }).on("error", (err) => {
+      reject({
+        statusCode: 500,
+        body: `Error fetching image: ${err.message}`,
+      });
+    });
+  });
 };
