e1558ebe4748d2a544a558927b25674d6e32356c
岳芳 <779622836@qq.com>
2026-08-07 09:34:28 +0800

    测试

--- api/xchatjson.js
+++ api/xchatjson.js
@@ -1,28 +1,42 @@
-const { Buffer } = require('buffer');
+const {
+	Buffer
+} = require('buffer');
 exports.handler = async (event, context) => {
-  const method = event.httpMethod;
-  const body = JSON.parse(event.body || '{}');
+	const method = event.httpMethod;
+	const body = JSON.parse(event.body || '{}');
 
-  let responseData;
-  let headers = {
-    'Content-Type': 'application/json'
-  };
+	let responseData;
+	let headers = {
+		'Content-Type': 'application/json'
+	};
 
-  if (method === 'POST') {
-    responseData = body; // 直接返回你指定的 JSON 对象
-  } else if (method === 'GET') {
-    responseData = { code: 0, data: 'GET 方法不支持' };
-  } else {
-    return {
-      statusCode: 405,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ code: 1, message: 'Method Not Allowed' })
-    };
-  }
+	if (method === 'POST') {
+		responseData = {
+			code: 0,
+			data: '666666666',
+			message: null
+		};
+	} else if (method === 'GET') {
+		responseData = {
+			code: 0,
+			data: 'GET 方法不支持'
+		};
+	} else {
+		return {
+			statusCode: 405,
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				code: 1,
+				message: 'Method Not Allowed'
+			})
+		};
+	}
 
-  return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify(responseData)
-  };
+	return {
+		statusCode: 200,
+		headers,
+		body: JSON.stringify(responseData)
+	};
 };
